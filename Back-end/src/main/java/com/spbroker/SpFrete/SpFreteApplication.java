b50b39f0aff990093a975ab126f2a96fc93f75bd
Lucas Rodrigues <rs.lucas1@gmail.com>
2021-01-13 17:36:43 -0300

    criacao do projeto

---
+++ Back-end/src/main/java/com/spbroker/SpFrete/SpFreteApplication.java
@@ -0,0 +1,13 @@
+package com.spbroker.SpFrete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpFreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpFreteApplication.class, args);
+	}
+
+}
